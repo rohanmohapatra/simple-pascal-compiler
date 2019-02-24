14a6abe0d209ce6a312e7b94603443876386ba9f
rohanmohapatra <rohanironcrest14@gmail.com>
2019-02-24 16:22:51 +0530

    Added Additional Input and Output Cases

--- src/input-test-case/helloWorld2.pas
+++ src/input-test-case/helloWorld2.pas
@@ -1,13 +1,22 @@
 program hello;
 uses crt;
 {* Here the main program block starts *}
-procedure drawLine; 
+procedure drawLine(x:integer); 
 {This procedure helps me to avoid the rewriting the for loops}
 var 
 	counter : integer;
 begin
 	writeln("Hello, World!");
 end;
+function max(num2: integer): integer;
+var
+   {* local variable declaration *}
+   result: integer;
+
+begin
+   max := result;
+end;
+
 begin
 	writeln("Hello, World!");
 end. 
---
+++ src/input-test-case/helloWorld3.pas
@@ -0,0 +1,15 @@
+program hello;
+uses crt;
+{* Here the main program block starts *}
+function max(num1,num2: integer): integer;
+var
+   {* local variable declaration *}
+   result: integer;
+
+begin
+   max := result;
+end;
+
+begin
+	writeln("Hello, World!");
+end. 
