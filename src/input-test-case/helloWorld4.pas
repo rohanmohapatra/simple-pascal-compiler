cb35467cc9dfc83f05e1283700cca1eae3d89882
RohanMohapatra <rohan.ron14@gmail.com>
2019-02-25 15:21:18 +0530

    Check

---
+++ src/input-test-case/helloWorld4.pas
@@ -0,0 +1,35 @@
+program hello;
+uses crt;
+type
+	uReal = real;
+	uString = string;
+	uInteger = integer;
+{* Here the main program block starts *}
+function max(num1,num2: integer): integer;
+var
+   {* local variable declaration *}
+   result: integer;
+   function mMac: string;
+   var 
+   		inMMac : string;
+   		result: uString;
+   begin
+   end;
+begin
+   max := result;
+end;
+
+function min(num1,num2: integer): integer;
+var
+	result : uInteger;
+	{num2 : integer;}
+
+begin
+	min := result;
+end;
+
+begin
+	for i:= 1 to 10 do writeln("Hello");
+	while (x>0) do x:=5;
+	writeln("Hello, World!");
+end. 
