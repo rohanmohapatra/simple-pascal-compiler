b95decc01e92be378c92c1d332ce667b62e1f9f1
RohanMohapatra <rohan.ron14@gmail.com>
2019-03-14 21:46:32 +0530

    Test Cases

---
+++ src/input-test-case/test1.pas
@@ -0,0 +1,31 @@
+program hello;
+uses crt,hello, pokemon;
+const
+PI = 3.1415;
+{ Single Comment }
+type
+{* This is
+Multi
+Comment*}
+hello,testr = integer;
+testtesttesttesttesttesttesttest,what = real;
+whatis = string;
+one,two,three = boolean;
+var
+a,b:integer;
+c:integer;
+one,two,three:boolean;
+test : whatis;
+test2 : one;
+test3,test4 : what;
+d : integer;
+begin
+	a:=10;
+	b+=20;
+	c := "Hello";
+	test3:=56.69;
+	a:=a+10;
+	c := (a+b);
+	d := a-c+b;
+	writeln("Printable");
+end.
---
+++ src/input-test-case/test2.pas
@@ -0,0 +1,40 @@
+program hello;
+uses crt;
+type
+	uReal = real;
+	uString = string;
+	uInteger = integer;
+{* Here the main program block starts *}
+var
+	i : integer;
+	x : integer;
+	y : array [9...25] of boolean;
+function max(num1,num2: integer): integer;
+var
+   {* local variable declaration *}
+   result: integer;
+   max : integer;
+begin
+   max := result;
+end;
+
+function min(num1,num2: integer): integer;
+var
+	result : uInteger;
+	num3 : integer;
+	min : integer;
+
+begin
+	min := result;
+end;
+
+begin
+		x:=x+2;
+	for i:= 1 to 10 
+	do 
+		writeln("Hello");
+		x:=4;
+	while (x>0) do x:=5;
+	writeln("Hello, World!");
+
+end. 
