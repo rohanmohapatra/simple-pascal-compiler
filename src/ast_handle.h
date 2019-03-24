d483f1f0cb59a464a4c38a81a18ae5f00427e579
RohanMohapatra <rohan.ron14@gmail.com>
2019-03-24 23:09:27 +0530

    Added Headers

--- src/ast_handle.h
+++ src/ast_handle.h
@@ -13,6 +13,41 @@ struct ast_node // for binary/unary operators and expression lists
   struct ast_node * right;
 };
 
+struct ast_root_node // for Root Node
+{
+  int node_type;
+
+  struct ast_node *program_node;
+
+  struct ast_node *block_node;
+};
+
+struct ast_block_node // for Block Node
+{
+  int node_type;
+
+  struct ast_node *uses_node;
+
+  struct ast_node *constant_node;
+
+  struct ast_node *type_node;
+
+  struct ast_node *variable_node;
+
+  struct ast_node *function_node;
+
+  struct ast_node *procedure_node;
+
+  struct ast_node *execution_node;
+};
+
+struct ast_program_node // for Root Node
+{
+  int node_type;
+
+  char* program_name;
+};
+
 struct ast_relational_node // for relational operators
 {
   int node_type;
@@ -58,4 +93,23 @@ struct ast_assignment_node // for assignment expressions
 
   struct ast_node * value;
 };
+
+void print_initial_tree(struct ast_node *root,int level);
+void print_tree(struct ast_node *root);
+
+struct ast_node *new_ast_root_node (
+  struct ast_node *program_node,
+  struct ast_node *block_node);
+
+struct ast_node *new_ast_block_node (
+  struct ast_node *uses_node,
+  struct ast_node *constant_node,
+  struct ast_node *type_node,
+  struct ast_node *variable_node,
+  struct ast_node *function_node,
+  struct ast_node *procedure_node,
+  struct ast_node *execution_node);
+
+struct ast_node *new_ast_program_node (
+  char* program_name);
 
