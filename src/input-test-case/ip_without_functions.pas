a8a4348ee8b6106e23d7698163f7b37a1807fc92
RohanMohapatra <rohan.ron14@gmail.com>
2019-04-02 11:41:17 +0530

    Latest Commit

---
+++ src/input-test-case/ip_without_functions.pas
@@ -0,0 +1,34 @@
+program hello;
+uses crt,hello, pokemon;
+const
+PI = 3.1415;
+{ Single Comment }
+type
+{* This is
+Multi
+Comment*}
+hello,testr = integer;
+testtesttesttesttesttesttesttest,what = real;
+whatis = string;
+one,two,three = boolean;
+var
+a,b:integer;
+c:integer;
+one,two,three:boolean;
+test : whatis;
+test2 : one;
+test3,test4 : what;
+d : integer;
+begin
+	a :=10;
+	b+=20;
+	test := "Hello";
+	test3:=56.69;
+	a := 10+40;
+	a:=a+10;
+	a :=test3+4;
+	c := (a+test3);
+	d := a-c+b;
+	d:=10+10;
+	writeln("Printable");
+end.
